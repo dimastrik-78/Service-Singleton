64df3374ccb1684c6b3359265ec544595287b289
dimastrik-78 <78548398+dimastrik-78@users.noreply.github.com>
2022-10-21 12:43:36 +0300

    v3.404x5

--- Atomic Reactor/Assets/_Source/EnrichmentAndDecayTimes.cs
+++ Atomic Reactor/Assets/_Source/EnrichmentAndDecayTimes.cs
@@ -1,13 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using Resources;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "NewRadioactiveResources", menuName = "SO/Enrichment And Decay Times")]
 public class EnrichmentAndDecayTimes : ScriptableObject
 {
+    [SerializeField] private RadioactiveResourcesType resourcesType;
     [SerializeField] private float enrichmentTime;
     [SerializeField] private float decayTime;
 
+    public RadioactiveResourcesType ResourcesType => resourcesType;
     public float EnrichmentTime => enrichmentTime;
     public float DecayTime => decayTime;
 }
--- Atomic Reactor/Assets/_Source/SingletonTimerEnrichmentAndDecay.cs
+++ Atomic Reactor/Assets/_Source/SingletonTimerEnrichmentAndDecay.cs
@@ -4,5 +4,14 @@ using UnityEngine;
 
 public class SingletonTimerEnrichmentAndDecay
 {
+    private Object[] items;
+    private EnrichmentAndDecayTimes _resource;
     
+    
+    
+    private void GettingResource()
+    {
+        items = UnityEngine.Resources.LoadAll("RadRes", typeof(EnrichmentAndDecayTimes));
+        _resource = (EnrichmentAndDecayTimes)items[0];
+    }
 }
